8b1114cb7b36a4af6a37af5290be136b0645711b
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-06-15 17:28:05 +0900

    working

---
+++ mod.ts
@@ -0,0 +1,115 @@
+declare global {
+  namespace JSX {
+    interface Element {
+      nodeKey: typeof nodeKey;
+      type: string;
+      props?: { [prop: string]: string } | null;
+      children?: ChildNode[];
+    }
+    interface IntrinsicElements {
+      [elemName: string]: unknown;
+    }
+    // interface ElementChildrenAttribute {
+    //   children: {};
+    // }
+  }
+}
+
+type Node =
+  | JSX.Element
+  | string
+  | number
+  | null
+  | false;
+type ChildNode =
+  | Node
+  | Promise<Node>
+  | Node[]
+  | Promise<Node[]>
+  | Promise<Promise<Node>[]>;
+
+const nodeKey = Symbol("[nodeKey]");
+// deno-lint-ignore no-explicit-any
+function isJSXElement(arg: any): arg is JSX.Element {
+  return arg.nodeKey === nodeKey;
+}
+
+export function h(
+  type: string,
+  props?: { [prop: string]: string } | null,
+  ...children: ChildNode[]
+): JSX.Element {
+  return { nodeKey, type, props, children };
+}
+
+const escape: Record<string, string> = {
+  "<": "&lt;",
+  ">": "&gt;",
+  "&": "&amp;",
+  "'": "&#39;",
+  '"': "&#34;",
+};
+function escapeHTML(text: string): string {
+  return text.replaceAll(/[&<>"']/g, (char) => {
+    return escape[char];
+  });
+}
+
+async function renderChildren(children: ChildNode): Promise<string> {
+  const awaitedChildren = await children;
+  if (Array.isArray(awaitedChildren)) {
+    const rendered = await Promise.all(awaitedChildren.map(renderChildren));
+    return rendered.join("");
+  }
+  if (!awaitedChildren) {
+    return "";
+  }
+  if (isJSXElement(awaitedChildren)) {
+    return await renderToString(awaitedChildren);
+  }
+  return escapeHTML(`${awaitedChildren}`);
+}
+
+const singleTag = new Set([
+  "area",
+  "base",
+  "basefont",
+  "br",
+  "col",
+  "embed",
+  "hr",
+  "img",
+  "input",
+  "keygen",
+  "link",
+  "meta",
+  "param",
+  "source",
+  "spacer",
+  "track",
+  "wbr",
+]);
+/** Renders a given JSX node to a string. */
+export async function renderToString(
+  { type, props, children }: JSX.Element,
+): Promise<string> {
+  // render props
+  const _props = props
+    ? Object.entries(props)
+      .map(([key, value]) => ` ${key}="${value.replace(/\"/g, '\\"')}"`)
+      .join("")
+    : "";
+
+  let innerHTML = "";
+  if (children) {
+    for (const child of children) {
+      innerHTML += await renderChildren(child);
+    }
+  }
+
+  if (singleTag.has(type)) {
+    return `<${type}${_props} />`;
+  } else {
+    return `<${type}${_props}>${innerHTML}</${type}>`;
+  }
+}
---
+++ test.tsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+import { h, renderToString } from "./mod.ts";
+
+const a = (
+  <a onload="val">
+    {Promise.resolve([[<a foo="val"></a>], Promise.resolve("aaaa")])}
+  </a>
+);
+// {[<a foo="val">a</a>, Promise.resolve("<hey>")]}
+// <div>{Promise.resolve(<h></h>)}</div>
+console.log(a);
+console.log(await renderToString(a));
